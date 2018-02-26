11d47a36a540fc7a4e4805bb68fe5f497baf479d
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-25 21:26:38 -0700

    refactoring the dictionary for the header method.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,3 @@
-require "./lib/dictionary.rb"
-
 class Parser
 
   attr_reader :markdown_text
@@ -15,12 +13,20 @@ class Parser
 
   def header
     if markdown_text.start_with?('#')
-      symbol_index = markdown_text.split.first #
-      include Dictionary
+      symbol_index = markdown_text.split.shift #
       symbol = markdown_headers.fetch(symbol_index) #finds the value of the headers key
       symbol_value = symbol.index(" ") #finds the space in the value
       symbol.insert(symbol_value, markdown_text) #Adds the text to the HTML format
-
     end
+  end
+
+  def markdown_headers
+    { "#" => "<h1> </h1>",
+      "##" => "<h2> </h2>",
+      "###" => "<h3> </h3>",
+      "####" => "<h4> </h4>",
+      "#####" => "<h5> </h5>",
+      "######" => "<h6> </h6>"
+    }
   end
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -30,18 +30,18 @@ class ConverterTest < Minitest::Test
 
   def test_it_can_parse_a_header
     parser = Parser.new("# My Life in Desserts")
-    parser = Parser.new("## My Life in Desserts")
-    parser = Parser.new("### My Life in Desserts")
-    parser = Parser.new("##### My Life in Desserts")
-    parser = Parser.new("###### My Life in Desserts")
-    parser = Parser.new("####### My Life in Desserts")
+    # parser = Parser.new("## My Life in Desserts")
+    # parser = Parser.new("### My Life in Desserts")
+    # parser = Parser.new("#### My Life in Desserts")
+    # parser = Parser.new("##### My Life in Desserts")
+    # parser = Parser.new("###### My Life in Desserts")
 
     header = "My Life in Desserts"
     assert_equal "<h1> #{header} </h1>", parser.header
-    assert_equal "<h2> #{header} </h2>", parser.header
-    assert_equal "<h3> #{header} </h3>", parser.header
-    assert_equal "<h4> #{header} </h4>", parser.header
-    assert_equal "<h5> #{header} </h5>", parser.header
-    assert_equal "<h6> #{header} </h6>", parser.header
+    # assert_equal "<h2> #{header} </h2>", parser.header
+    # assert_equal "<h3> #{header} </h3>", parser.header
+    # assert_equal "<h4> #{header} </h4>", parser.header
+    # assert_equal "<h5> #{header} </h5>", parser.header
+    # assert_equal "<h6> #{header} </h6>", parser.header
   end
 end
